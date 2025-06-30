fe51fcc6bb9c75e6a5dac30e0d77a2642b9c4899
danie <danny-rickers@gmail.com>
2025-06-30 22:48:56 +0200

    Routes....

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,22 @@
+// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+const routes = [
+  { path: '/',           name: 'home',       component: HomeView },
+  { path: '/home',       redirect: { name: 'home' } },
+  { path: '/who-we-are', name: 'who-we-are', component: HomeView },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes,
+  scrollBehavior(to) {
+    if (to.name === 'who-we-are') {
+      return { el: '#who-we-are', behavior: 'smooth' }
+    }
+    return { top: 0 }
+  },
 })
 
 export default router
